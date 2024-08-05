e9888ce74e50a77df3cd9e224557239703a375fe
Adit0507 <adi05072002@gmail.com>
2024-08-05 13:34:09 +0530

    extracting node text

--- parse.go
+++ parse.go
@@ -1,8 +1,8 @@
 package link
 
 import (
-	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -21,11 +21,42 @@ func Parse(r io.Reader) ([]Link, error) {
 	}
 
 	nodes := linkNodes(doc)
+	var links []Link
 	for _, node := range nodes {
-		fmt.Println(node)
+		links = append(links, buildLink(node))
 	}
 
-	return nil, nil
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr{
+		if attr.Key == "href"{
+			ret.Href = attr.Val
+			break
+		}
+	}
+
+	ret.Text= text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+
+	if n.Type != html.ElementNode{
+		return ""
+	}
+
+	var ret string
+	for c := n.FirstChild; c != nil; c =c.NextSibling{
+		ret += text(c) + " "
+	}
+
+	return strings.Join(strings.Fields(ret), " ")
 }
 
 func linkNodes(n *html.Node) []*html.Node {
